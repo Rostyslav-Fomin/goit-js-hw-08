80853dd01e44b3c4330a104053e297eefe1d7e7a
Rostyslav-Fomin <142998754+Rostyslav-Fomin@users.noreply.github.com>
2024-05-05 12:07:08 +0300

    corrected task#1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
 console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const items = galleryItems
+  .map(
+    ({ description, original, preview }) =>
+      `<li class="gallery__item" style="list-style: none"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}"></a></li>`
+  )
+  .join('');
+galleryList.insertAdjacentHTML('beforeend', items);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox', function () {
+
+  });
