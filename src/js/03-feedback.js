e8ecbd571ba62f9c38ca7da8385b1c594626fd58
Rostyslav-Fomin <142998754+Rostyslav-Fomin@users.noreply.github.com>
2024-05-07 09:36:30 +0300

    corrected task #3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,15 +3,13 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const formInput = document.querySelector('input');
 const formTextarea = document.querySelector('textarea');
-const formData = {};
 const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
 
 recoveryUserData();
 
-form.addEventListener('submit', throttle(onSubmit, 500));
+form.addEventListener('submit', onSubmit);
 form.addEventListener('input', throttle(onInput, 500));
-
-
 
 function onInput(evt) {
   const user = evt.target;
@@ -21,22 +19,27 @@ function onInput(evt) {
 
 function onSubmit(evt) {
   evt.preventDefault();
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-  evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+  if (formData['email'] && formData['message']) {
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    evt.currentTarget.reset();
+    formData = {};
+    localStorage.removeItem(STORAGE_KEY);
+  }
 }
 
 function recoveryUserData() {
   const savedUserData = localStorage.getItem(STORAGE_KEY);
-	const parsedUserData = JSON.parse(savedUserData);
-	if (parsedUserData) {
-		const email = parsedUserData.email;
-	const message = parsedUserData.message;
-	if (email) {
-		formInput.value = email;
-	} if (message) {
-		formTextarea.value = message; 
-	}
-	}
-	
+  const parsedUserData = JSON.parse(savedUserData);
+  if (parsedUserData) {
+    const email = parsedUserData.email;
+    const message = parsedUserData.message;
+    formData['email'] = email;
+    formData['message'] = message;
+    if (email) {
+      formInput.value = email;
+    }
+    if (message) {
+      formTextarea.value = message;
+    }
+  }
 }
